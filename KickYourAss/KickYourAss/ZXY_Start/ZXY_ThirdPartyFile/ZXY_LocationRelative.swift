cea032f5bcd40becaa96227828409a0324bf439a
ChinaStart <zxy_start@outlook.com>
2015-02-13 16:00:41 +0800

    Map Change After

--- KickYourAss/KickYourAss/ZXY_Start/ZXY_ThirdPartyFile/ZXY_LocationRelative.swift
+++ KickYourAss/KickYourAss/ZXY_Start/ZXY_ThirdPartyFile/ZXY_LocationRelative.swift
@@ -9,11 +9,88 @@
 import UIKit
 import MapKit
 
+
+typealias ZXY_LocationRelativeFailureBlock = (errorMessage : String) -> Void
 private let _locationR = ZXY_LocationRelative()
+
+
+
 class ZXY_LocationRelative: NSObject {
+    
+    var failureBlock : ZXY_LocationRelativeFailureBlock?
+    
+    
+    private var previousTimeStamp : NSTimeInterval = 0
+    
+    private let locationService : BMKLocationService = BMKLocationService()
+    private let locationGeo     : BMKGeoCodeSearch   = BMKGeoCodeSearch()
+    
+    
+    private var currentUserLocation : BMKUserLocation?
+    private var currentLocationInfo : BMKReverseGeoCodeResult?
+    
     class var  sharedInstance: ZXY_LocationRelative
     {
         return _locationR
+    }
+    
+    func startLocateUserPosition()
+    {
+        locationService.delegate = self
+        locationService.startUserLocationService()
+        locationGeo.delegate = self
+        
+    }
+    
+    func sendLocationToEveryBody() -> CLLocation?
+    {
+        return currentUserLocation?.location
+    }
+    
+    func sendCityNameToEveryBody() -> BMKReverseGeoCodeResult?
+    {
+        return currentLocationInfo
+    }
+}
+
+extension ZXY_LocationRelative : BMKLocationServiceDelegate , BMKGeoCodeSearchDelegate
+{
+    func didUpdateBMKUserLocation(userLocation: BMKUserLocation!) {
+        currentUserLocation = userLocation
+        var currentTimeStamp = NSTimeIntervalSince1970
+        if(currentTimeStamp - previousTimeStamp > 5)
+        {
+            var geoOption  = BMKReverseGeoCodeOption()
+            geoOption.reverseGeoPoint = userLocation.location.coordinate
+            var flag : Bool = locationGeo.reverseGeoCode(geoOption)
+            if(flag)
+            {
+                previousTimeStamp = currentTimeStamp
+            }
+            else
+            {
+                previousTimeStamp = 0
+            }
+        }
+    }
+    
+    func didFailToLocateUserWithError(error: NSError!) {
+        var failMessage = ""
+        switch error.code
+        {
+        case CLError.Denied.rawValue :
+            self.failureBlock?(errorMessage: "请在权限设置中开启定位功能")
+        case CLError.Network.rawValue:
+            self.failureBlock?(errorMessage: "请连接网络")
+        default:
+            return
+        }
+    }
+    
+    func onGetReverseGeoCodeResult(searcher: BMKGeoCodeSearch!, result: BMKReverseGeoCodeResult!, errorCode error: BMKSearchErrorCode) {
+        currentLocationInfo = result
+        previousTimeStamp = NSTimeIntervalSince1970
+        
     }
 }
 
@@ -26,8 +103,5 @@ extension ZXY_LocationRelative
         return BDCoorDe
     }
     
-//    func bdToGPS(bdCoor : CLLocationCoordinate2D) -> CLLocationCoordinate2D
-//    {
-//        
-//    }
+
 }
